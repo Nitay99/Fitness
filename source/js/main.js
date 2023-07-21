7435e2d8ee052bb778a633516dcaa0b0aa62886f
Nitay <trakktttor@gmail.com>
2023-07-21 15:42:13 +0700

    Делает начальную настройку слайдера

--- source/js/main.js
+++ source/js/main.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // Vendor
   // ---------------------------------
   Swiper.use([Navigation]);
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.slider', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
@@ -22,7 +22,27 @@ window.addEventListener('DOMContentLoaded', () => {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
   });
+
   initTabs();
 
   // Utils
@@ -38,6 +58,36 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    const swiper = new Swiper('.reviews__slider', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: false,
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      slidesPerView: 1,
+      spaceBetween: 10,
+      // Responsive breakpoints
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+        1200: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+      },
+    });
     const form = new Form();
     window.form = form;
     form.init();
