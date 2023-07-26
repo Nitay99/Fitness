758e205fe5f37f32f3c40ae34400b981abbd37b2
Nitay <trakktttor@gmail.com>
2023-07-26 14:25:51 +0700

    Поправляет ПП

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -1,10 +1,16 @@
 const video = document.querySelector('.video');
 const buttonPlay = document.querySelector('.video__button');
+const userAgent = navigator.userAgent.toLowerCase();
+const browChrome = /chrome/.test(userAgent);
 
 const createFrame = () => {
   if (video) {
     let fr = document.createElement('iframe');
-    fr.src = 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1';
+    if (browChrome) {
+      fr.src = 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1&mute=1';
+    } else {
+      fr.src = 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1';
+    }
     fr.setAttribute('frameborder', '0');
     fr.setAttribute('width', '100%');
     fr.setAttribute('height', '100%');
